b32dc411407ee11e77b81136322952a8a4acd506
Luís <lf@void.pt>
2017-08-18 17:32:34 +0100

    second commit

---
+++ test/components/comment_box_test.js
@@ -0,0 +1,37 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox', () => {
+    let component;
+
+    beforeEach(() => {
+        component = renderComponent(CommentBox);
+    });
+
+    it('Has the correct class', () => {
+        expect(component).to.have.class('comment-box');
+    });
+
+    it('Has a text area', () => {
+        expect(component.find('textarea')).to.exist;
+    });
+
+    it('Has a button', () => {
+        expect(component.find('button')).to.exist;
+    });
+
+    describe('entering some text', () => {
+        beforeEach(() => {
+            component.find('textarea').simulate('change', 'new comment');
+        });
+
+        it('Shows that text in the text area', () => {
+            expect(component.find('textarea')).to.have.value('new comment');
+        });
+
+        it('when submitted, clears the input', () => {
+            component.simulate('submit');
+            expect(component.find('textarea')).to.have.value('');
+        });
+    });
+});
